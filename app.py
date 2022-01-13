5f919c989cbfa137095775309165f42838e2027a
purusharma168 <123atrt@gmail.com>
2022-01-13 23:33:52 +0530

    API created model file created html file created test the workflow

--- app.py
+++ app.py
@@ -1,16 +1,28 @@
-import logging
 import numpy as np
+import logging
 from flask import Flask, request, jsonify, render_template
-import pandas as pd
 import pickle
-import os
 
 app = Flask(__name__)
+model = pickle.load(open('model.pkl', 'rb'))
 
 
 @app.route('/')
 def home():
-    return "hello"
+    return render_template('index.html')
+
+
+@app.route('/predict', methods=['POST'])
+def predict():
+
+    features = [int(x) for x in request.form.values()]
+    final_features = [np.array(features)]
+    prediction = model.predict(final_features)
+
+    output = round(prediction[0], 1)
+
+    return render_template('index.html', prediction_text='Your Rating is: {}'.format(output))
+
 
 if __name__ == "__main__":
     app.run(debug=True)
