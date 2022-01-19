1e40444d69af32486960b4b6facfc833b521645d
purusharma168 <123atrt@gmail.com>
2022-01-19 22:04:59 +0530

    updated app file

--- app.py
+++ app.py
@@ -25,5 +25,5 @@ def predict():
 
 
 if __name__ == "__main__":
-    #app.run(port=8000,debug=True)
-    app.run(debug=True)  # running the app
+    app.run(port=5000, debug=True)
+    #app.run(debug=True)  # running the app
